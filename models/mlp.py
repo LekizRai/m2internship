dcffbd325aaa926dd8d280779c5f2338ba38c5a6
LekizRai <nguyenvuhoangtruong@gmail.com>
2026-05-05 14:42:13 +0200

    Add MLP module

---
+++ models/mlp.py
@@ -0,0 +1,71 @@
+import torch
+from torch import nn
+
+
+class MLP(nn.Module):
+    def __init__(
+            self,
+            input_dim: int,
+            output_dim: int,
+            hidden_dims: list[int],
+            hidden_activation: nn.Module | None = None,
+            output_activation: nn.Module | None = None,
+            bias: bool = True,
+            is_normalized: bool = False,
+    ):
+        super().__init__()
+
+        # Initialize layers and attributes
+        self.input_layer = None
+        self.hidden_layers = []
+        self.output_layer = None
+        self.normalization_layer = None
+        self.hidden_activation = hidden_activation
+        self.output_activation = output_activation
+
+        # Define layers
+        if len(hidden_dims) == 0:
+            self.input_layer = nn.Linear(
+                in_features=input_dim,
+                out_features=output_dim,
+                bias=bias,
+            )
+        else:
+            self.input_layer = nn.Linear(
+                in_features=input_dim,
+                out_features=hidden_dims[0],
+                bias=bias,
+            )
+            for i in range(len(hidden_dims[1:])):
+                self.hidden_layers.append(nn.Linear(
+                        in_features=hidden_dims[i],
+                        out_features=hidden_dims[i+1],
+                        bias=bias,
+                    )
+                )
+            self.output_layer = nn.Linear(
+                in_features=hidden_dims[-1],
+                out_features=output_dim,
+                bias=bias,
+            )
+
+        # Define normalization layer
+        if is_normalized:
+            self.normalization_layer = nn.LayerNorm(normalized_shape=output_dim)
+
+    def forward(self, x):
+        x = self.input_layer(x)
+
+        for hidden_layer in self.hidden_layers:
+            x = hidden_layer(x)
+            if self.hidden_activation is not None:
+                x = self.hidden_activation(x)
+
+        if self.output_layer is not None:
+            x = self.output_layer(x)
+        if self.normalization_layer is not None:
+            x = self.normalization_layer(x)
+        if self.output_activation is not None:
+            x = self.output_activation(x)
+
+        return x
---
+++ tests/test_mlp.py
@@ -0,0 +1,32 @@
+import torch
+from torch import nn
+
+from models.mlp import MLP
+
+
+
+def test_mlp():
+    B = 64
+    I = 12
+    O = 128
+    batch = torch.randn(B, I)
+
+    # Test MLP without hidden layer
+    model = MLP(
+        input_dim=I,
+        output_dim=O,
+        hidden_dims=[],
+        hidden_activation=nn.ReLU(),
+    )
+    prediction = model(batch)
+    assert prediction.shape == (B, O)
+
+    # Test MLP with hidden layers
+    model = MLP(
+        input_dim=I,
+        output_dim=O,
+        hidden_dims=[3, 17, 5, 77, 35],
+        output_activation=nn.Softmax(),
+    )
+    prediction = model(batch)
+    assert prediction.shape == (B, O)
