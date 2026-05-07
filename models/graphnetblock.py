5801fa6d2b783c364d85e9bab97304d3f4f49f6e
LekizRai <nguyenvuhoangtruong@gmail.com>
2026-05-07 16:24:14 +0200

    Add GraphNetBlock and test

--- models/graphnetblock.py
+++ models/graphnetblock.py
@@ -1,3 +1,4 @@
+from typing import Dict, Any
 import torch
 from torch import nn
 
@@ -6,7 +7,7 @@ from models.mlp import MLP
 
 
 class GraphNetBlock(nn.Module):
-    """Module to do message passing"""
+    """Module for message passing"""
 
     def __init__(
             self,
@@ -15,20 +16,124 @@ class GraphNetBlock(nn.Module):
         super().__init__()
         self._config = config
 
-        # Initialize MLPs for all types of edge (mesh, contact, ...) feature update
+        # Initialize MLPs for all types of edge (mesh, contact, ...) feature updates
         self._edge_mlps = {}
+        for edge_type in config.edge_types:
+            self._edge_mlps[edge_type] = MLP(
+                # Input dimension = encoded (sender (node) + receiver (node) + current edge) feature dimensions
+                # Note: all features are encoded to the same latent space before
+                input_dim=config.latent_dim * 3,
+                output_dim=config.latent_dim,
+                hidden_dims=config.hidden_dims,
+                hidden_activation=nn.ReLU(),
+                is_output_normalized=True,
+            )
 
         # Initialize MLP for node feature update
-        self._node_mlp = None
+        self._node_mlp = MLP(
+            # Input dimension = encoded (aggregated all types of corresponding toward edges + current node) feature dimensions
+            # Note: all features are encoded to the same latent space before
+            input_dim=config.latent_dim * (len(config.edge_types) + 1),
+            output_dim=config.latent_dim,
+            hidden_dims=config.hidden_dims,
+            hidden_activation=nn.ReLU(),
+            is_output_normalized=True,
+        )
 
         # Initialize MLP for tetrahedral feature update
-        self._tetra_mlp = None
+        self._tetra_mlp = MLP(
+            # Input dimension = encoded (4 vertex nodes + tetrahedral) feature dimensions
+            # Note: all features are encoded to the same latent space before
+            input_dim=config.latent_dim * 5,
+            output_dim=config.latent_dim,
+            hidden_dims=config.hidden_dims,
+            hidden_activation=nn.ReLU(),
+            is_output_normalized=True,
+        )
 
-    def _update_node(self, x):
-        pass
+    def _update_node_features(self, batch: Dict[str, Any]) -> torch.Tensor:
+        V = batch["nodes.features"].shape[-2]  # Number of nodes
 
-    def _update_edge(self, x):
-        pass
+        # Edge features aggregation
+        features = [batch["nodes.features"]]
+        for edge_type in self._config.edge_types:
+            edges = batch[edge_type]
+            edge_features = batch[edge_type + ".features"]
+            E = edges.shape[-2] # Number of edges
+            D_E = edge_features.shape[-1] # Dimension of (encoded) edge feature (= latent dimension in TacGraspNet)
+            aggregated_edge_features = torch.zeros(V, D_E) # Initialize with zero tensor
+            aggregated_edge_features.scatter_add_(
+                dim=-2,
+                index=edges[..., 1, None].expand(E, D_E), # Consider only toward edges
+                src=edge_features
+            )
+            features.append(aggregated_edge_features)
 
-    def forward(self, x):
-        return x
+        # Update node features with MLP
+        return self._node_mlp(torch.cat(features, dim=-1))
+
+    def _update_edge_features(self, edge_type: str, batch: Dict[str, Any]) -> torch.Tensor:
+        node_features = batch["nodes.features"]
+        edges = batch[edge_type]
+        edge_features = batch[edge_type + ".features"]
+        E = edges.shape[-2]  # Number of edges
+        D_V = node_features.shape[-1]  # Dimension of (encoded) node feature (= latent dimension in TacGraspNet)
+
+        # Node features gathering
+        sender_features = torch.gather(
+            input=node_features,
+            dim=-2,
+            index=edges[..., 0, None].expand(E, D_V)
+        )
+        receiver_features = torch.gather(
+            input=node_features,
+            dim=-2,
+            index=edges[..., 1, None].expand(E, D_V)
+        )
+        features = [sender_features, receiver_features, edge_features]
+
+        # Update edge features with MLP
+        return self._edge_mlps[edge_type](torch.cat(features, dim=-1))
+
+    def _update_tetra_features(self, batch: Dict[str, Any]) -> torch.Tensor:
+        node_features = batch["nodes.features"]
+        tetras = batch["tetrahedra"]
+        T = batch["tetrahedra.features"].shape[-2] # Number of tetrahedra
+        D_V = node_features.shape[-1]  # Dimension of (encoded) node feature (= latent dimension in TacGraspNet)
+
+        # Node features gathering
+        features = [batch["tetrahedra.features"]]
+        for i in range(4):
+            features.append(torch.gather(
+                input=node_features,
+                dim=-2,
+                index=tetras[..., i, None].expand(T, D_V)
+            ))
+
+        # Update edge features with MLP
+        return self._tetra_mlp(torch.cat(features, dim=-1))
+
+    # TODO
+    # def _update_global_node_feature(self, batch: Dict[str, Any]):
+    #     return batch["globa_node.features"]
+
+    def forward(self, batch: Dict[str, Any]) -> Dict[str, Any]:
+        new_batch = batch.copy()
+
+        # Update edge features
+        for edge_type in self._config.edge_types:
+            new_batch[edge_type + ".features"] = self._update_edge_features(edge_type, batch)
+
+        # Update node features
+        new_batch["nodes.features"] = self._update_node_features(batch)
+
+        # Update tetrahedral features
+        new_batch["tetrahedra.features"] = self._update_tetra_features(batch)
+
+        # Add residual connections
+        new_batch["nodes.features"] += batch["nodes.features"]
+        for edge_type in self._config.edge_types:
+            new_batch[edge_type + ".features"] += batch[edge_type + ".features"]
+        new_batch["tetrahedra.features"] += batch["tetrahedra.features"]
+
+        return new_batch
--- tests/test_graphnetblock.py
+++ tests/test_graphnetblock.py
@@ -8,44 +8,32 @@ from models.tacgraspnet.tacgraspnet_config import TacGraspNetConfig
 
 def test_graphnetblock():
     # Define sample data
-    V = 5 # Number of nodes
-    E_MESH = 18 # Number of mesh edges
+    V = 8 # Number of nodes
+    E_MESH = 24 # Number of mesh edges
     E_CONTACT = 2 # Number of contact edges (or world edges)
     T = 2 # Number of tetrahedra
 
-    # Dimension of node features
-    # 6 = node velocity (3) + number of node types (3)
-    V_FEATURE = 6
+    D_V = 128 # Dimension of (encoded) node features
+    D_E_MESH = 128 # Dimension of (encoded) mesh edge features
+    D_E_CONTACT = 128 # Dimension of (encoded) contact edge (world edge) features
+    D_T = 128 # Dimension of (encoded) tetrahedral features
 
-    # Dimension of mesh edge features. There are two options
-    # 4 = relative displacement in template (rest object) (3) + its norm (1)
-    E_MESH_FEATURE = 4
-    # # 8 = relative displacement in first and second frame (6) + their norm (2)
-    # E_MESH_FEATURE = 8
-
-    # Dimension of contact edge (world edge) features
-    # 5 = relative displacement in current frame (3) + its norm (1) + applied force in contact (1)
-    E_CONTACT_FEATURE = 5
-
-    # Dimension of tetrahedral features
-    # 1 = stress (1)
-    T_FEATURE = 1
     batch = {
-        "nodes.features": torch.rand(V, V_FEATURE),
+        "nodes.features": torch.rand(V, D_V),
         "mesh_edges": torch.tensor(
-            [[0, 1], [0, 2], [0, 3], [1, 2], [2, 3], [1, 3], [1, 4], [2, 4],[3, 4],
-             [1, 0], [2, 0], [3, 0], [2, 1], [3, 2], [3, 1], [4, 1], [4, 2], [4, 3]]
+            [[0, 1], [1, 2], [2, 0], [0, 3], [1, 3], [2, 3], [4, 5], [5, 6], [4, 6], [4, 7], [5, 7], [6, 7],
+             [1, 0], [2, 1], [0, 2], [3, 0], [3, 1], [3, 2], [5, 4], [6, 5], [6, 4], [7, 4], [7, 5], [7, 6]]
         ).long(),
         "contact_edges": torch.tensor(
-            [[0, 4], [4, 0]]
+            [[3, 7], [7, 3]]
         ).long(),
-        "mesh_edges.features": torch.rand(E_MESH, E_MESH_FEATURE),
-        "contact_edges.features": torch.rand(E_CONTACT, E_CONTACT_FEATURE),
+        "mesh_edges.features": torch.rand(E_MESH, D_E_MESH),
+        "contact_edges.features": torch.rand(E_CONTACT, D_E_CONTACT),
         "tetrahedra": torch.tensor(
             [[0, 1, 2, 3],
-             [1, 2, 3, 4]]
+             [4, 5, 6, 7]]
         ),
-        "tetrahedra.features": torch.rand(T, T_FEATURE),
+        "tetrahedra.features": torch.rand(T, D_T),
     }
 
     # Test forward
@@ -53,3 +41,7 @@ def test_graphnetblock():
     model = GraphNetBlock(config)
     batch = model.forward(batch)
     assert isinstance(batch, Dict)
+    assert batch["nodes.features"].shape == (V, D_V)
+    assert batch["mesh_edges.features"].shape == (E_MESH, D_E_MESH)
+    assert batch["contact_edges.features"].shape == (E_CONTACT, D_E_CONTACT)
+    assert batch["tetrahedra.features"].shape == (T, D_T)
