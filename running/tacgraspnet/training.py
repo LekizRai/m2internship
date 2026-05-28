Fix output normalization error in training mode and add score logs

running/tacgraspnet/training.py
@@ -1,3 +1,5 @@
+import torch
+
 from tqdm import tqdm
 from torch.optim import AdamW
 from torch.utils.data import random_split, DataLoader
@@ -47,17 +49,40 @@ def sample_train():
 
     # Initialize loss and score functions
     loss_fn = MSE(model_config)
+    score_classes = [DisplacementMAE, StressMAE, DisplacementR2PerSample, StressR2PerSample]
+    score_fns = {}
+    for score_class in score_classes:
+        score_fns[score_class] = score_class(model_config)
 
     for epoch in range(model_config.n_epochs):
+        # Initialize model
         model.train()
 
-        for batch in tqdm(train_loader, mininterval=5.0):
+        # Initialize lists of losses and scores
+        train_losses = []
+        train_scores = {}
+        for score_class in score_classes:
+            train_scores[score_class] = []
+
+        # Training model
+        for batch in tqdm(train_loader, mininterval=5.0, leave=False):
+            # Optimizing model
             optimizer.zero_grad()
             batch = preprocessor(batch)
             batch = model(batch)
             loss = loss_fn(batch)
             loss.backward()
             optimizer.step()
+
+            # Append losses and scores to lists
+            train_losses.append(loss.cpu().detach())
+            for score_class in score_classes:
+                train_scores[score_class].append(score_fns[score_class](batch).cpu().detach())
+
+        # Print mean values of losses and scores as progress
+        print("Epoch:", epoch + 1, "| Loss:", torch.tensor(train_losses).mean())
+        for score_class in score_classes:
+            print(str(score_fns[score_class]) + ":", torch.tensor(train_scores[score_class]).mean())
 
 
 if __name__ == "__main__":

src/models/tacgraspnet/tacgraspnet.py
@@ -158,28 +158,31 @@ class TacGraspNet(nn.Module):
                 )
 
                 # Normalize target node outputs (target displacements)
-                batch["targets.nodes.outputs"] = batch["vertices.positions"] - starting_pos # Target displacements
+                batch["targets.nodes.outputs"] = batch["vertices.positions"] - starting_pos  # Target displacements
                 # Store only normalized tactile sensor node outputs
                 batch["targets.tactile_sensors.nodes.normalized_outputs"] = self._node_output_normalizer(
                     # Only normalize tactile sensor node outputs
-                    batch["targets.nodes.outputs"][batch["nodes.types"] != NodeType.OBJECT]
+                    batch["targets.nodes.outputs"][batch["nodes.types"] != NodeType.OBJECT],
+                    is_training=self._config.is_training
                 )
 
                 # Normalize target tetrahedral outputs (target stresses)
                 batch["targets.tetrahedra.outputs"] = batch["tetrahedra.stresses"]
                 batch["targets.tetrahedra.normalized_outputs"] = self._tetra_output_normalizer(
-                    batch["targets.tetrahedra.outputs"]
+                    batch["targets.tetrahedra.outputs"],
+                    is_training=self._config.is_training
                 )
-            else: # Normalize corresponding outputs otherwise
+            else:  # Normalize corresponding outputs otherwise
                 batch["targets.nodes.outputs"] = torch.cat([
                     batch["vertices.positions"] - starting_pos,
                     batch["vertices.stresses"]
                 ], dim=-1)
                 # Store only normalized tactile sensor node outputs
-                batch["targets.tactile_sensors.nodes.normalized_outputs"] = self._node_output_normalizer([
+                batch["targets.tactile_sensors.nodes.normalized_outputs"] = self._node_output_normalizer(
                     # Only normalize tactile sensor node outputs
-                    batch["targets.nodes.outputs"][batch["nodes.types"] != NodeType.OBJECT]
-                ])
+                    batch["targets.nodes.outputs"][batch["nodes.types"] != NodeType.OBJECT],
+                    is_training=self._config.is_training
+                )
 
         # Encode
         batch = self._encode(batch)