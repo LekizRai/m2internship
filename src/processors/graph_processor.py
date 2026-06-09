5ccef0274ce024f58c320737356c85c57b3e04a9
LekizRai <nguyenvuhoangtruong@gmail.com>
2026-06-09 11:03:46 +0200

    Check errors

--- src/processors/graph_processor.py
+++ src/processors/graph_processor.py
@@ -167,8 +167,7 @@ class GraphBuildingProcessor(Processor):
 
         return contact_edges, contact_edge_features
 
-    @staticmethod
-    def _build_node_features(batch: Databatch) -> torch.Tensor:
+    def _build_node_features(self, batch: Databatch) -> torch.Tensor:
         # Get node velocities of tactile sensors and object
         # Note that velocities are computed using normal vectors of tactile sensors (left and right)
         node_velocities = [] # Initialize node velocity list
@@ -186,7 +185,7 @@ class GraphBuildingProcessor(Processor):
                 batch["tactile_sensors.normals"][idx, 1, ...],
                 (n_ts_comp_nodes, 1)
             ) # Set right tactile sensor node velocities as right tactile sensor normal
-            obj_node_velocities = torch.zeros((n_obj_nodes, 3)) # Set object node velocities as zeros
+            obj_node_velocities = torch.zeros((n_obj_nodes, 3), device=self._config.device) # Set object node velocities as zeros
 
             # Add velocities to node velocity list
             node_velocities.extend([
