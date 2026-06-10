b23a8763a25c650cb76c90d340baf55a4d1dff40
LekizRai <nguyenvuhoangtruong@gmail.com>
2026-06-10 20:41:31 +0200

    Add TacGraspNet v0.3

--- src/models/tacgraspnet/tacgraspnet_processor.py
+++ src/models/tacgraspnet/tacgraspnet_processor.py
@@ -6,9 +6,9 @@ from processors.device_processor import DeviceProcessor
 
 def make_tacgraspnet_processors(config: TacGraspNetConfig) -> tuple[Processor, Processor]:
     preprocessor = PipelineProcessor([
-        DeviceProcessor(config.device),
-        GraphBuildingProcessor(config),
-        # DeviceProcessor(config.device),
+        DeviceProcessor(config.device), # Put every tensor to GPU for accelerating graph building process
+        GraphBuildingProcessor(config), # Build graph
+        DeviceProcessor(config.device), # Put every tensor to GPU for model forwarding
     ])
-    postprocessor = PipelineProcessor([])
+    postprocessor = PipelineProcessor([]) # Do nothing here
     return preprocessor, postprocessor
