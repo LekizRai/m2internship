a4bc4cfc16f75a969f8843f2aefce4c4310f8cdc
LekizRai <nguyenvuhoangtruong@gmail.com>
2026-06-10 15:53:16 +0200

    Fix %h as home directory error

--- src/processors/graph_processor.py
+++ src/processors/graph_processor.py
@@ -199,7 +199,7 @@ class GraphBuildingProcessor(Processor):
                 batch["tactile_sensors.normals"][idx, 1, ...],
                 (n_ts_comp_nodes, 1)
             ) # Set right tactile sensor node velocities as right tactile sensor normal
-            obj_node_velocities = torch.zeros((n_obj_nodes, 3), device="cpu") # Set object node velocities as zeros
+            obj_node_velocities = torch.zeros((n_obj_nodes, 3), device="cuda") # Set object node velocities as zeros
 
             # Add velocities to node velocity list
             node_velocities.extend([
